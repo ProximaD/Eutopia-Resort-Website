a179f053c52d69af0523204c5556409eee062b4c
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2024-09-14 15:41:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,5 +19,22 @@ showMenu('nav-toggle', 'nav-menu')
 /*=============== SWIPER JS ===============*/
 
 /*================== POPUP ===============*/
+const btnOpenVideo = document.querySelectorAll('.islands__video-content')
+const islandsPopup = document.getElementById('popup')
+
+function poPup(){
+    islandsPopup.classList.add('show-popup')
+}
+
+btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
+
+const btnCloseVideo = document.getElementById('popup-close')
+
+btnCloseVideo.addEventListener('click', ()=>{
+    islandsPopup.classList.remove('show-popup')
+})
 
 /*============= GSAP ANIMATION ===========*/
+
+
+
