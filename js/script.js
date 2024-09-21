21c8f73720edba3e2d87d3eac7f805e58f3e8267
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2024-09-21 20:08:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,3 +48,22 @@ btnCloseVideo.addEventListener('click', ()=>{
 })
 
 /*============= GSAP ANIMATION ===========*/
+const controlImg = document.querySelectorAll('.controls__img')
+
+function scrollAnimation(){
+    gsap.from('.islands__subtitle', {opacity: 0, duration: .2, delay: .2, y: -20})
+    gsap.from('.islands__title', {opacity: 0, duration: .3, delay: .3, y: -20})
+    gsap.from('.islands__description', {opacity: 0, duration: .4, delay: .4, y: -20})
+    gsap.from('.islands__button', {opacity: 0, duration: .5, delay: .5, y: -20})
+    gsap.from('.islands__video-content', {opacity: 0, duration: .6, delay: .6, y: -20})
+
+    islandsPopup.classList.remove('show-popup')
+}
+
+controlImg.forEach(c => c.addEventListener('click', scrollAnimation))
+
+
+
+
+
+
